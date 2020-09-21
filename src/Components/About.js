dce2652e6fc24bcd40f975e27f909fff0aacde5f
Łukasz Mazurkiewicz <mazurkiewicz_l@outlook.com>
2020-09-21 17:40:24 +0200

    START Translation

--- src/Components/About.js
+++ src/Components/About.js
@@ -23,12 +23,12 @@ class About extends Component {
             <img className="profile-pic"  src={profilepic} alt="Tim Baker Profile Pic" />
          </div>
          <div className="nine columns main-col">
-            <h2>About Me</h2>
+            <h2>Kilka słów o mnie...</h2>
 
             <p>{bio}</p>
             <div className="row">
                <div className="columns contact-details">
-                  <h2>Contact Details</h2>
+                  <h2>Kontakt</h2>
                   <p className="address">
 						   <span>{name}</span><br />
 						   <span>{street}<br />
